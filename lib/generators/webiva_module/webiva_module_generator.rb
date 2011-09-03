08e5e6437283e2e4ba580b84c33a81283da72696
Doug Youch <doug@sessionm.com>
2011-09-03 15:30:27 -0400

    use rails 3 generators

--- lib/generators/webiva_module/webiva_module_generator.rb
+++ lib/generators/webiva_module/webiva_module_generator.rb
@@ -3,29 +3,24 @@
 
 
 
-class WebivaModuleGenerator < Rails::Generator::NamedBase
-  attr_accessor :class_title
+class WebivaModuleGenerator < Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)
+  argument :name, :type => :string
 
-  def manifest 
-    @name = @name.underscore
+  attr_accessor :class_title, :class_name
 
-    @class_title = class_name.underscore.humanize.titleize
+  def copy_config_files
+    @name = name.underscore
+    @class_name = @name.classify
+    @class_title = @class_name.underscore.humanize.titleize
 
-    record do |m|
-      m.directory "/vendor/modules/#{@name}"
-      m.file "init.rb","/vendor/modules/#{@name}/init.rb"
+    copy_file "init.rb", "vendor/modules/#{@name}/init.rb"
       
-      m.directory "/vendor/modules/#{@name}/app/controllers/#{@name}"
-      m.directory "/vendor/modules/#{@name}/app/models"
-      m.directory "/vendor/modules/#{@name}/app/views/#{@name}/admin"
+    empty_directory "vendor/modules/#{@name}/app/controllers/#{@name}"
+    empty_directory "vendor/modules/#{@name}/app/models"
+    empty_directory "vendor/modules/#{@name}/app/views/#{@name}/admin"
       
-      m.template "admin_controller.rb","/vendor/modules/#{@name}/app/controllers/#{@name}/admin_controller.rb"
-      m.template "options.rhtml","/vendor/modules/#{@name}/app/views/#{@name}/admin/options.rhtml"
-    end
-  
-  end
-
-  def banner
-    "Usage: #{$0} #{spec.name} <module name>"
+    template "admin_controller.rb", "vendor/modules/#{@name}/app/controllers/#{@name}/admin_controller.rb"
+    template "options.rhtml", "vendor/modules/#{@name}/app/views/#{@name}/admin/options.rhtml"
   end
 end
