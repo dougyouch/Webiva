e7c2471d2e67de578070530d6e12f1a1a6158c00
Doug Youch <doug@sessionm.com>
2012-07-11 10:16:45 -0400

    Upgrade to ruby 1.9.3-p194

--- config/initializers/date_patch.rb
+++ config/initializers/date_patch.rb
@@ -2,10 +2,30 @@ require 'time'
 
 class Date
   class << self
-    alias :org_parse :_parse
+    alias :org_internal_parse :_parse
 
     # switch the month and day field for expecting US dates
     def _parse(str, comp=true)
+      str = str.sub(/(\d{1,2})\/(\d{1,2})\/(\d{1,4})/, '\2/\1/\3')
+      org_internal_parse str, comp
+    end
+
+    alias :org_parse :parse
+
+    # switch the month and day field for expecting US dates
+    def parse(str, comp=true)
+      str = str.sub(/(\d{1,2})\/(\d{1,2})\/(\d{1,4})/, '\2/\1/\3')
+      org_parse str, comp
+    end
+  end
+end
+
+class DateTime
+  class << self
+    alias :org_parse :parse
+
+    # switch the month and day field for expecting US dates
+    def parse(str, comp=true)
       str = str.sub(/(\d{1,2})\/(\d{1,2})\/(\d{1,4})/, '\2/\1/\3')
       org_parse str, comp
     end
